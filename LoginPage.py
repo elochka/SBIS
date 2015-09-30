e604d0d8c580364942e26efe1534a29d8ca8aa22
elochka <akulinaem@gmail.com>
2015-10-01 00:26:04 +0300

    Initial commit

---
+++ LoginPage.py
@@ -0,0 +1,15 @@
+__author__ = 'Asus'
+class LoginPage:
+    def __init__(self, driver, loginField="#fld-loginName", passwordField="#fld-loginPass", loginButton="#logButton"):
+        self.driver = driver
+        self.loginField = loginField
+        self.passwordField = passwordField
+        self.loginButton = loginButton
+    def getLoginField(self):
+        return self.driver.find_element_by_css_selector(self.loginField)
+    def getPasswordInput(self):
+        return self.driver.find_element_by_css_selector(self.passwordField)
+    def getloginButton(self):
+        return self.driver.find_element_by_css_selector(self.loginButton)
+
+
---
+++ homework.py
@@ -0,0 +1,27 @@
+__author__ = 'Asus'
+
+from LoginPage import LoginPage
+import logging
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+#задали формат записи лога
+logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
+
+driver = webdriver.Chrome()
+driver.get("http://fix-inside.tensor.ru")
+time.sleep(5)
+assert "Вход в систему/СБИС" in driver.title
+logging.info("Loaded main page. Logining...")
+LogPage = LoginPage(driver)
+LogPage.getLoginField().send_keys("check_rigth_user")
+LogPage.getPasswordInput().send_keys("qwerty123")
+LogPage.getloginButton().click()
+
+time.sleep(5) #wait for next page load
+logging.info("Login process started")
+assert "СБИС" in driver.title
+
+logging.info ("Login was successfull")
+driver.close()
