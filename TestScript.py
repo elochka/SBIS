9ecaa25c1304785d26eaf70674977119ba911d89
elochka <akulinaem@gmail.com>
2015-10-01 12:10:16 +0300

    скрипт запускающий тесты

--- TestScript.py
+++ TestScript.py
@@ -1,4 +1,5 @@
 __author__ = 'Asus'
+
 import unittest
 from LoginPage import LoginPage
 import logging
@@ -8,12 +9,13 @@ from selenium.webdriver.common.keys import Keys
 
 class SimplisticTest(unittest.TestCase):
     def test(self):
-        #задали формат записи лога
+        # формат записи лога
         logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
         driver = webdriver.Chrome()
         driver.get("http://fix-inside.tensor.ru")
         time.sleep(5)
-        assert "Вход в систему/СБИС" in driver.title
+        #assert "Вход в систему/СБИС" in driver.title
+        self.assertEqual("Вход в систему/СБИС", driver.title)
         logging.info("Loaded main page. Logining...")
         LogPage = LoginPage(driver)
         LogPage.getLoginField().send_keys("check_rigth_user")
@@ -21,7 +23,8 @@ class SimplisticTest(unittest.TestCase):
         LogPage.getloginButton().click()
         time.sleep(5) #wait for next page load
         logging.info("Login process started")
-        assert "СБИС" in driver.title
+        #assert "СБИС" in driver.title
+        self.assertEqual("СБИС", driver.title)
         logging.info ("Login was successfull")
         driver.close()
 
