c0bd515cdb76125e4db12fc35f6a4775c7ef169c
elochka <akulinaem@gmail.com>
2015-10-01 11:56:51 +0300

    скрипт запускающий тесты

--- TestScript.py
+++ TestScript.py
@@ -9,26 +9,22 @@ from selenium.webdriver.common.keys import Keys
 class SimplisticTest(unittest.TestCase):
     def test(self):
         self.assertTrue(True)
-
-#задали формат записи лога
-logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
-
-driver = webdriver.Chrome()
-driver.get("http://fix-inside.tensor.ru")
-time.sleep(5)
-assert "Вход в систему/СБИС" in driver.title
-logging.info("Loaded main page. Logining...")
-LogPage = LoginPage(driver)
-LogPage.getLoginField().send_keys("check_rigth_user")
-LogPage.getPasswordInput().send_keys("qwerty12")
-LogPage.getloginButton().click()
-
-time.sleep(5) #wait for next page load
-logging.info("Login process started")
-assert "СБИС" in driver.title
-
-logging.info ("Login was successfull")
-driver.close()
+        #задали формат записи лога
+        # logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
+        driver = webdriver.Chrome()
+        driver.get("http://fix-inside.tensor.ru")
+        time.sleep(5)
+        assert "Вход в систему/СБИС" in driver.title
+        logging.info("Loaded main page. Logining...")
+        LogPage = LoginPage(driver)
+        LogPage.getLoginField().send_keys("check_rigth_user")
+        LogPage.getPasswordInput().send_keys("qwerty123")
+        LogPage.getloginButton().click()
+        time.sleep(5) #wait for next page load
+        logging.info("Login process started")
+        assert "СБИС" in driver.title
+        logging.info ("Login was successfull")
+        driver.close()
 
 if __name__ == '__main__':
     unittest.main()
