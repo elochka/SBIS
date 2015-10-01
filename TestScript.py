d40138d1d6765353f94506edf2c79e727183705f
elochka <akulinaem@gmail.com>
2015-10-01 12:26:39 +0300

    скрипт c тестом

--- TestScript.py
+++ TestScript.py
@@ -12,17 +12,17 @@ class SimplisticTest(unittest.TestCase):
         logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
         driver = webdriver.Chrome()
         driver.get("http://fix-inside.tensor.ru")
+        logging.info("Loading login page.")
         time.sleep(5)
         self.assertEqual("Вход в систему/СБИС", driver.title)
-        logging.info("Loaded main page. Logining...")
         LogPage = LoginPage(driver)
         LogPage.getLoginField().send_keys("check_rigth_user")
-        LogPage.getPasswordInput().send_keys("qwerty12")
+        LogPage.getPasswordInput().send_keys("qwerty123")
         LogPage.getloginButton().click()
-        time.sleep(5) #wait for next page load
         logging.info("Login process started")
-        self.assertEqual("СБИС", driver.title
-        logging.info ("Login was successfull")
+        time.sleep(5) #wait for next page load
+        self.assertEqual("СБИС", driver.title)
+        logging.info("Login was successfull")
         driver.close()
 
 if __name__ == '__main__':
