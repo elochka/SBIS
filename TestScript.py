182a8b98ae03fb69d466945614732da7c30128c0
elochka <akulinaem@gmail.com>
2015-10-01 11:30:10 +0300

    скрипт запускающий тесты

---
+++ TestScript.py
@@ -0,0 +1,10 @@
+__author__ = 'Asus'
+import unittest
+
+class SimplisticTest(unittest.TestCase):
+
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
