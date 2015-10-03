775428347f271e8afddcfebea5abb4a52bfde241
elochka <akulinaem@gmail.com>
2015-10-03 22:57:25 +0300

    refactoring

--- TestScript.py
+++ TestScript.py
@@ -10,52 +10,57 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
 class LoginPageTest(unittest.TestCase):
-    def test(self):
-        logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
-        driver = webdriver.Chrome()
-        wait10sec = WebDriverWait(driver, 10)
-        driver.get("http://fix-inside.tensor.ru")
-        logging.info("Loading login page.")
-        time.sleep(5)
-        self.assertEqual("Вход в систему/СБИС", driver.title)
+    def setUp(self):
+        logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
+        self.log = logging.getLogger(__name__)
+        self.driver = webdriver.Chrome()
+        #self.driver.set_window_size(1280, 1024)
+        self.wait10sec = WebDriverWait(self.driver, 10)
 
-        logPage = LoginPage(driver)
-        logPage.getLoginField().send_keys("check_rigth_user")
-        logPage.getPasswordInput().send_keys("qwerty123")
-        logPage.getloginButton().click()
-        logging.info("Login process started")
-        time.sleep(5)
-        self.assertEqual("СБИС", driver.title)
-        logging.info("Login was successfull")
+    def tearDown(self):
+        self.driver.close()
 
-        mainPage = MainPage(driver)
-        wait10sec.until(lambda _: len(mainPage.getClosePopupLinks()) > 0)
+    def waitAndClosePopup(self):
+        mainPage = MainPage(self.driver)
+        self.wait10sec.until(lambda _: len(mainPage.getClosePopupLinks()) > 0)
         for link in mainPage.getClosePopupLinks():
             logging.info("Popup was closed")
             link.click()
             time.sleep(1)
 
+
+    def test_main(self):
+        self.driver.get("http://fix-inside.tensor.ru")
+        self.log.info("Loading login page.")
+        time.sleep(5)
+        self.assertEqual("Вход в систему/СБИС", self.driver.title)
+
+        logPage = LoginPage(self.driver)
+        logPage.getLoginField().send_keys("check_rigth_user")
+        logPage.getPasswordInput().send_keys("qwerty123")
+        logPage.getloginButton().click()
+        self.log.info("Login process started")
+        time.sleep(5)
+        self.assertEqual("СБИС", self.driver.title)
+        self.log.info("Login was successfull")
+
+        self.waitAndClosePopup()
+
+        mainPage = MainPage(self.driver)
         mainPage.getstaffLink().click()
         time.sleep(2)
         mainPage.getstaffLink().click()
         time.sleep(5)
-        self.assertEqual("Сотрудники/СБИС", driver.title)
-        logging.info("Staff page opened")
+        self.assertEqual("Сотрудники/СБИС", self.driver.title)
+        self.log.info("Staff page opened")
 
-        staffPage = StaffPage(driver)
+        staffPage = StaffPage(self.driver)
         #firstName = staffPage.getorgLink().text()
         staffPage.getorgLink().click()
         time.sleep(5)
-        logging.info("Organizations list opened")
+        self.log.info("Organizations list opened")
         # проверка: открылась форма с подразделениями
 
-
-
-
-
-
-
-        driver.close()
 
 if __name__ == '__main__':
     unittest.main()
