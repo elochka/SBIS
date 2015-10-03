bd6f0d051c95b63f0ce3d65cb2fb46c99a699585
elochka <akulinaem@gmail.com>
2015-10-03 21:44:59 +0300

    скрипт c тестом

---
+++ SbisPage.py
@@ -0,0 +1,7 @@
+__author__ = 'Asus'
+class SbisPage:
+    def __init__(self, driver, staffLink='div[data-id="staff"] > a'):
+        self.driver = driver
+        self.staffLink = staffLink
+    def getstaffLink(self):
+        return self.driver.find_element_by_css_selector(self.staffLink)
