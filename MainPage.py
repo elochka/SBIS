f29edbde072113fb059f562622bdd851a2f74564
elochka <akulinaem@gmail.com>
2015-10-03 22:49:13 +0300

    popup closing

--- MainPage.py
+++ MainPage.py
@@ -3,5 +3,10 @@ class MainPage:
     def __init__(self, driver, staffLink='div[data-id="staff"] > a'):
         self.driver = driver
         self.staffLink = staffLink
+
     def getstaffLink(self):
         return self.driver.find_element_by_css_selector(self.staffLink)
+
+    def getClosePopupLinks(self):
+        locator = "a.ws-window-titlebar-action.close"
+        return self.driver.find_elements_by_css_selector(locator)
--- TestScript.py
+++ TestScript.py
@@ -7,27 +7,34 @@ from StaffPage import StaffPage
 import logging
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
 class LoginPageTest(unittest.TestCase):
     def test(self):
         logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
         driver = webdriver.Chrome()
+        wait10sec = WebDriverWait(driver, 10)
         driver.get("http://fix-inside.tensor.ru")
         logging.info("Loading login page.")
         time.sleep(5)
         self.assertEqual("Вход в систему/СБИС", driver.title)
+
         logPage = LoginPage(driver)
         logPage.getLoginField().send_keys("check_rigth_user")
         logPage.getPasswordInput().send_keys("qwerty123")
         logPage.getloginButton().click()
         logging.info("Login process started")
-        time.sleep(5) #wait for next page load
+        time.sleep(5)
         self.assertEqual("СБИС", driver.title)
         logging.info("Login was successfull")
-        time.sleep(5)
 
         mainPage = MainPage(driver)
+        wait10sec.until(lambda _: len(mainPage.getClosePopupLinks()) > 0)
+        for link in mainPage.getClosePopupLinks():
+            logging.info("Popup was closed")
+            link.click()
+            time.sleep(1)
+
         mainPage.getstaffLink().click()
         time.sleep(2)
         mainPage.getstaffLink().click()
@@ -36,9 +43,12 @@ class LoginPageTest(unittest.TestCase):
         logging.info("Staff page opened")
 
         staffPage = StaffPage(driver)
+        #firstName = staffPage.getorgLink().text()
         staffPage.getorgLink().click()
         time.sleep(5)
         logging.info("Organizations list opened")
+        # проверка: открылась форма с подразделениями
+
 
 
 
